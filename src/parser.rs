37b5852169d1c935493a7992d65b41a9598ff2f1
alapanbagchi <alapanbagchi.dev@gmail.com>
2026-03-23 23:47:00 +0530

    feat(parser): implement manual cli argument parsing

--- src/parser.rs
+++ src/parser.rs
@@ -1,1 +1,42 @@
 use crate::command::Command;
+
+pub fn parse_args(args: Vec<String>) -> Result<Command, String> {
+    if args.is_empty() {
+        return Err("No command provided".into());
+    }
+
+    let command = args[0].as_str();
+
+    match command {
+        "add" => {
+            if args.len() < 2 {
+                return Err("Missing task title".into());
+            }
+
+            Ok(Command::Add {
+                title: args[1].clone(),
+            })
+        }
+        "list" => Ok(Command::List),
+
+        "done" => {
+            if args.len() < 2 {
+                return Err("Missing task ID".into());
+            }
+
+            let id = args[1].parse::<u32>().map_err(|_| "Invalid ID")?;
+            Ok(Command::Done { id })
+        }
+        "delete" => {
+            if args.len() < 2 {
+                return Err("Missing task ID".into());
+            }
+
+            let id = args[1].parse::<u32>().map_err(|_| "Invalid ID")?;
+
+            Ok(Command::Delete { id })
+        }
+
+        _ => Err("Unknown Command".into()),
+    }
+}
