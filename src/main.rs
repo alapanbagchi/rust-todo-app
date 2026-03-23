d31424afbe3f0fc4125a4c1547ce7d6a81d6826e
alapanbagchi <alapanbagchi.dev@gmail.com>
2026-03-24 01:44:03 +0530

    feat(core): Added the parser to main

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,17 @@ mod parser;
 
 use std::env;
 
+use crate::parser::parse_args;
+
 fn main() {
     let args: Vec<String> = env::args().skip(1).collect();
+
+    match parse_args(args) {
+        Ok(command) => {
+            println!("Parsed command {:?}", command);
+        }
+        Err(err) => {
+            println!("ERROR: {err}");
+        }
+    }
 }
