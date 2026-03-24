5f4339b8f8fc9e5bf3f8edf3abc533dac85de242
alapanbagchi <alapanbagchi.dev@gmail.com>
2026-03-24 16:52:43 +0530

    feat(core): Command adding system implemented in main

--- src/main.rs
+++ src/main.rs
@@ -8,11 +8,15 @@ use crate::{manager::TaskManager, parser::parse_args};
 
 fn main() {
     let args: Vec<String> = env::args().skip(1).collect();
-    let task_manager = TaskManager::new();
+    let mut task_manager = TaskManager::new();
     match parse_args(args) {
         Ok(command) => match command {
             command::Command::List => {
                 task_manager.list_tasks();
+            }
+            command::Command::Add { title } => {
+                task_manager.add_task(&title);
+                println!("Task successfully added!")
             }
             _ => eprintln!("ERROR: Command not implemented yet"),
         },
