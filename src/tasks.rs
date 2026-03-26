f4688bc553b360ba429de0178d8d8a0381b98df7
alapanbagchi <alapanbagchi.dev@gmail.com>
2026-03-26 17:10:35 +0530

    feat(core): enable serialization and deserialization for task struct

--- src/tasks.rs
+++ src/tasks.rs
@@ -1,6 +1,10 @@
+use serde::{Deserialize, Serialize};
 use uuid::Uuid;
 
+#[derive(Serialize, Deserialize)]
+#[serde(rename_all = "PascalCase")]
 pub struct Task {
+    #[serde(rename = "ID")]
     pub id: Uuid,
     pub title: String,
     pub completed: bool,
